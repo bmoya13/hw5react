e7692d1295d8df828e3c3d5d92a49cdf102edf48
bmoya13 <bimoya@cpp.edu>
2023-05-05 00:18:58 -0700

    change gitignore, add secret key

--- src/component/GiveawaysPS.jsx
+++ src/component/GiveawaysPS.jsx
@@ -10,7 +10,7 @@ const client = axios.create({
     'sort-by': 'value'
    },
    headers: {
-      'X-RapidAPI-Key': '8525bc412bmsh5b9d590be0c05a6p1c493djsn36c213fb51f9',
+      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
       'X-RapidAPI-Host': 'gamerpower.p.rapidapi.com'
     }
  });
--- src/component/Worths.jsx
+++ src/component/Worths.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 const client = axios.create({
    baseURL: "https://gamerpower.p.rapidapi.com/api/worth",
    headers: {
-      'X-RapidAPI-Key': '8525bc412bmsh5b9d590be0c05a6p1c493djsn36c213fb51f9',
+      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
       'X-RapidAPI-Host': 'gamerpower.p.rapidapi.com'
     }
  });
